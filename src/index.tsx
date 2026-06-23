cf0714c9653566c15459c23dc00188b5cde23086
illi0125 <illikamalov10@gmail.com>
2026-06-24 00:06:06 +0900

    fix: MUI customized integration

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import { store } from './app/store';
 import App from './app/App';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
 import './css/index.css';
 import './css/app.css';
+import theme from './app/MaterialTheme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
