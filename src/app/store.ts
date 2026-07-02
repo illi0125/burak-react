af059ed464089b0a86d33cd0087b01acc16a38dc
illi0125 <illikamalov10@gmail.com>
2026-07-02 09:19:27 +0900

    feat: publish orderPage screen component

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import type { ThunkAction, Action } from "@reduxjs/toolkit";
 import HomePage from "./screens/homePage/index";
 import ProductsPage from "./screens/productsPage/productsPagesSlice";
-import OrdersPage from "./screens/ordersPage/index";
+import OrdersPage from "./screens/ordersPage/ordersSlice";
 import { createLogger } from "redux-logger";
 
 export const store = configureStore({
