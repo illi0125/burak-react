631b264bab5d137cba70ba288aebc2030643f1f1
illi0125 <illikamalov10@gmail.com>
2026-07-13 10:14:32 +0900

    fix: modify homePage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,7 +13,7 @@ import HelpPage from "./screens/helpPage";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
-import Test from "./screens/Test";
+// import Test from "./screens/Test";
 
 function App() {
   const location = useLocation();
@@ -36,8 +36,7 @@ function App() {
           <HelpPage />
         </Route>
         <Route exact path="/">
-        <Test/>
-          {/* <HomePage /> */}
+          <HomePage />
         </Route>
       </Switch>
       <Footer/>
