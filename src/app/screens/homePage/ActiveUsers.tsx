dc9ed40e4176bce898d0baa65b66937eebeabe4d
illi0125 <illikamalov10@gmail.com>
2026-07-23 02:46:49 +0900

    fix: develop top users and modify ActiveUsers component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -3,15 +3,19 @@ import Card from "@mui/joy/Card";
 import { CssVarsProvider, Typography } from "@mui/joy";
 import CardOverflow from "@mui/joy/CardOverflow";
 import AspectRatio from "@mui/joy/AspectRatio";
+import { createSelector } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+import { retrieveTopUsers } from "./selector";
+import { serverApi } from "../../../lib/config";
+import type { Member } from "../../../lib/types/member";
 
-const activeUsers = [
-  { memberNick: "Martin", memberImage: "/img/martin.webp" },
-  { memberNick: "Justin", memberImage: "/img/justin.webp" },
-  { memberNick: "Rose", memberImage: "/img/rose.webp" },
-  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
-];
+// ─── REDUX SLICE & SELECTOR ──────────────────────────
+const topUsersRetriever = createSelector(retrieveTopUsers, (topUsers) => ({
+  topUsers,
+}));
 
 export default function ActiveUsers() {
+  const { topUsers } = useSelector(topUsersRetriever);
   return (
     <div className="active-users-frame">
       <Container>
@@ -19,39 +23,42 @@ export default function ActiveUsers() {
           <Box className="category-title">Active Users</Box>
           <Stack className="cards-frame">
             <CssVarsProvider>
-              {activeUsers.length !== 0 ? (
-                activeUsers.map((user, index) => (
-                  <Card
-                    key={index}
-                    className="card"
-                    variant="outlined"
-                    sx={{ bgcolor: "background.body" }}
-                  >
-                    <CardOverflow>
-                      <AspectRatio ratio="1">
-                        <img
-                          src={user.memberImage}
-                          loading="lazy"
-                          alt={user.memberNick}
-                        />
-                      </AspectRatio>
-                    </CardOverflow>
-                    <CardOverflow
-                      variant="soft"
-                      sx={{
-                        bgcolor: "#fbfbfb",
-                        textAlign: "center",
-                        py: 1,
-                        // borderTop: "1px solid",
-                        borderColor: "divider",
-                      }}
+              {topUsers.length !== 0 ? (
+                topUsers.map((member: Member) => {
+                  const imagePath = `${serverApi}/${member.memberImage}`;
+                  return (
+                    <Card
+                      key={member._id}
+                      className="card"
+                      variant="outlined"
+                      sx={{ bgcolor: "background.body" }}
                     >
-                      <Typography className="member-nickname">
-                        {user.memberNick}
-                      </Typography>
-                    </CardOverflow>
-                  </Card>
-                ))
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                          <img
+                            src={imagePath}
+                            loading="lazy"
+                            alt={member.memberNick}
+                          />
+                        </AspectRatio>
+                      </CardOverflow>
+                      <CardOverflow
+                        variant="soft"
+                        sx={{
+                          bgcolor: "#fbfbfb",
+                          textAlign: "center",
+                          py: 1,
+                          // borderTop: "1px solid",
+                          borderColor: "divider",
+                        }}
+                      >
+                        <Typography className="member-nickname">
+                          {member.memberNick}
+                        </Typography>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
               ) : (
                 <Box className="no-data">No Active Users!</Box>
               )}
