c67f5c0ece51039b97a8fae83028c6bb87d45831
illi0125 <illikamalov10@gmail.com>
2026-07-28 03:40:38 +0900

    feat: create signup process

--- src/app/components/auth/index.tsx
+++ src/app/components/auth/index.tsx
@@ -7,6 +7,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import styled from "styled-components";
 import LoginIcon from "@mui/icons-material/Login";
 import { Modal, Backdrop, Fade, Stack, TextField, Fab } from "@mui/material";
+import type { T } from "../../../lib/types/common";
+import { Messages } from "../../../lib/config";
+import type { MemberInput } from "../../../lib/types/member";
+import MemberService from "../../services/MemberService";
+import { sweetErrorHandling } from "../../../lib/sweetAlert";
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -41,8 +46,49 @@ interface AuthenticationModalProps {
 export default function AuthenticationModal(props: AuthenticationModalProps) {
   const { signupOpen, loginOpen, handleSignupClose, handleLoginClose } = props;
   const classes = useStyles();
+  const [memberNick, setMemberNick] = useState<string>("");
+  const [memberPhone, setMemberPhone] = useState<string>("");
+  const [memberPassword, setMemberPassword] = useState<string>("");
 
   /** HANDLERS **/
+
+  const handleUsername = (e: T) => {
+    setMemberNick(e.target.value);
+  };
+  const handlePhone = (e: T) => {
+    setMemberPhone(e.target.value);
+  };
+  const handlePassword = (e: T) => {
+    setMemberPassword(e.target.value);
+  };
+  const handlePasswordKeyDown = (e: T) => {
+    if (e.key === "Enter" && signupOpen) {
+      handleSignupRequest().then();
+    }
+  };
+
+  const handleSignupRequest = async () => {
+    try {
+      console.log("inputs:", memberNick, memberPassword);
+      const isFulfill =
+        memberNick !== "" && memberPhone !== "" && memberPassword !== "";
+      if (!isFulfill) throw new Error(Messages.error3);
+
+      const signupInput: MemberInput = {
+        memberNick: memberNick,
+        memberPhone: memberPhone,
+        memberPassword: memberPassword,
+      };
+
+      const member = new MemberService();
+      const result = await member.signup(signupInput);
+      handleSignupClose();
+    } catch (err) {
+      console.log(err);
+      handleSignupClose();
+      sweetErrorHandling(err).then();
+    }
+  };
 
   return (
     <div>
@@ -53,6 +99,8 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
         open={signupOpen}
         onClose={handleSignupClose}
         closeAfterTransition
+        disableRestoreFocus
+        disableEnforceFocus
         BackdropComponent={Backdrop}
         BackdropProps={{
           timeout: 500,
@@ -72,22 +120,27 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
                 id="outlined-basic"
                 label="username"
                 variant="outlined"
+                onChange={handleUsername}
               />
               <TextField
                 sx={{ my: "17px" }}
                 id="outlined-basic"
                 label="phone number"
                 variant="outlined"
+                onChange={handlePhone}
               />
               <TextField
                 id="outlined-basic"
                 label="password"
                 variant="outlined"
+                onChange={handlePassword}
+                onKeyDown={handlePasswordKeyDown}
               />
               <Fab
                 sx={{ marginTop: "30px", width: "120px" }}
                 variant="extended"
                 color="primary"
+                onClick={handleSignupRequest}
               >
                 <LoginIcon sx={{ mr: 1 }} />
                 Signup
@@ -104,6 +157,8 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
         open={loginOpen}
         onClose={handleLoginClose}
         closeAfterTransition
+        disableRestoreFocus
+        disableEnforceFocus
         BackdropComponent={Backdrop}
         BackdropProps={{
           timeout: 500,
