6445f1e4f380423beaa827591dc0a2cc9d6d1c80
illi0125 <illikamalov10@gmail.com>
2026-07-28 02:32:37 +0900

    feat: publish authentication model component

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const serverApi: string = `${import.meta.env.VITE_API_URL}`;
 
-export const Message = {
+export const Messages = {
   error1: "Something went wrong!",
   error2: "Please login first!",
   error3: "Please fulfill all inputs!",
